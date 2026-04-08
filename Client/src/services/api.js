afc970e2ca27ede868da81f6d378f77c20c341e2
pragati singh <pragatisingh56786@gmail.com>
2026-04-08 19:51:50 +0530

    Update API base URL to production endpoint

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+  baseURL: process.env.REACT_APP_API_URL || 'https://train-booking-system-1-uxxn.onrender.com'
 });
 
 // Auto attach token to every request
